e0151f43a8cf98227bd8f291fe0b6ecb5a38d343
abheist <abhishek291093@gmail.com>
2022-06-08 23:00:51 -0300

    Add Codebuild codedpeloy spec

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -6,6 +6,20 @@ CLI_PROFILE=awsbootstrap
 
 EC2_INSTANCE_TYPE=t2.micro 
 
+AWS_AACOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
+  --query "Account" --output text`
+CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_AACOUNT_ID"
+
+echo -e "=============== Deploying setup.yml ============"
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME-setup \
+  --template-file setup.yml \
+  --no-file-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+
 # Deploy the CloudFormation template
 echo -e "\n\n=========== Deploying main.yml ==========="
 aws cloudformation deploy \
---
+++ start-service.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+source /home/ec2-user/.bash-profile
+cd /home/ec2-user/app/release
+npm run start
